5090a1eb7c3236f8a25276a58f75a1761ba445c2
Sandeep Desaraju <71257134+sandeep-dss@users.noreply.github.com>
2023-11-13 20:15:09 +0530

    Placeholder changes

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -55,7 +55,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => (
 
                 <div className='flex-1 flex flex-col gap-3'>
                   <div className='relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg'>
-                    <Image src="/hero.png" alt='car model' fill priority className='object-contain' />
+                    <Image src="/template.png" alt='car model' fill priority className='object-contain' />
                   </div>
 
                   <div className='flex gap-3'>
